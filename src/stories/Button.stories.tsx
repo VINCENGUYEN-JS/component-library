8fea0c442820863ce9ecc744782b9da3b7bf3981
VinceNguyen <nguyenquangv95@gmail.com>
2021-11-25 21:19:28 +0700

    Add Storybook environment

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,14 @@
+// File: src/stories/Button.stories.tsx
+
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { Button } from "../buttons/Button";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+} as Meta;
+
+const Template: Story = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
