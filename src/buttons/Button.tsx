35dcea18eceec5473c713df1e6c7469f107a68cf
VinceNguyen <nguyenquangv95@gmail.com>
2021-11-28 12:18:12 +0700

    Add styled-components for Button and Add prop-types support

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { StyledButton } from './styles';
 
-export const Button = React.forwardRef<
-  HTMLButtonElement,
-  React.ComponentPropsWithoutRef<'button'>
->(({ children, ...props }, ref) => {
-  return (
-    <button ref={ref} type="button" {...props}>
-      {children}
-    </button>
-  );
-});
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  variant?: 'primary' | 'secondary' | 'danger';
+  size?: 'small' | 'medium' | 'large';
+  isFullWidth?: boolean;
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, variant, size, isFullWidth, ...props }, ref) => {
+    return (
+      <StyledButton
+        ref={ref}
+        type="button"
+        variant={variant}
+        size={size}
+        isFullWidth={isFullWidth}
+        {...props}
+      >
+        {children}
+      </StyledButton>
+    );
+  }
+);
+
+Button.defaultProps = {
+  variant: 'primary',
+  size: 'medium',
+  isFullWidth: false,
+};
+
+Button.propTypes = {
+  variant: PropTypes.oneOf(['secondary', 'primary', 'danger']),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  isFullWidth: PropTypes.bool,
+};
 
 Button.displayName = 'Button';
